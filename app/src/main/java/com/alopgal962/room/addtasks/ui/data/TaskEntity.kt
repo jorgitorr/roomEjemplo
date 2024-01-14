ce4ed493db8827e3aaaa3bcf70304ec7a7f2a548
jorgitorr <jorgitorr97@gmail.com>
2024-01-14 18:09:57 +0100

    parte 2 4.3. Coroustines agregada

--- app/src/main/java/com/alopgal962/room/addtasks/ui/data/TaskEntity.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/data/TaskEntity.kt
@@ -1,2 +1,13 @@
 package com.alopgal962.room.addtasks.ui.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//La Entidad es el modelo de datos que vamos a persistir en nuestra base de datos...
+@Entity
+data class TaskEntity (
+    @PrimaryKey
+    val id: Int,
+    val task: String,
+    var selected: Boolean = false
+)
--- app/src/main/java/com/alopgal962/room/addtasks/ui/model/TaskModel.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/model/TaskModel.kt
@@ -2,8 +2,12 @@ package com.alopgal962.room.addtasks.ui.model
 
 //El valor del id por defecto lo vamos a calcular con el momento en el que lo creamos, es decir, el tiempo en milisegundos.
 //El valor de la casilla de verificación por defecto debería ser siempre falso cuando creamos la tarea.
+//Nuestro modelo de datos...
 data class TaskModel(
-    val id: Long = System.currentTimeMillis(),
+    //El método hashcode() nos crea un código único a través del número de milisegundos al que aplica...
+    //El hascode() es lo que utiliza el compilador para comparar objetos, es decir,
+    //lo hace a través del hashcode() que se genera en cada objeto.
+    val id: Int = System.currentTimeMillis().hashCode(),
     val task: String,
     var selected: Boolean = false
 )
