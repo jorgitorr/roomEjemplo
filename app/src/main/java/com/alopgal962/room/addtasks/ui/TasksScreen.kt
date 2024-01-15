b22dc535df5509721d16807afaac20a4ab095085
AdrianPRG <alopgal962@g.educaand.es>
2024-01-15 21:28:50 +0100

    empezar de nuevo

---
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TasksScreen.kt
@@ -0,0 +1,122 @@
+package com.alopgal962.room.addtasks.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.ViewModel
+
+/***
+ * Este fichero contendrá la función Composable básica principal con el mismo nombre que el fichero y el resto de funciones Composable que necesitemos para la creación de nuestro diseño final
+ */
+
+@Composable
+fun TasksScreen(tasksViewModel: TasksViewModel) {
+    //Para poder alinear el botón abajo a la derecha del contenedor, le pasamos el modifier con la alineación desde su contenedor...
+    Box(modifier = Modifier.fillMaxSize()) {
+        FabDialog(
+            Modifier.align(Alignment.BottomEnd))
+    }
+}
+
+
+@Composable
+fun FabDialog(
+    modifier: Modifier
+) {
+    FloatingActionButton(
+        onClick = {
+            //Mostrar diálogo para añadir una tarea
+        }, modifier = modifier.padding(16.dp)
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "")
+    }
+}
+
+
+@Composable
+fun TasksScreen(tasksViewModel: TasksViewModel) {
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
+    val myTaskText: String by tasksViewModel.myTaskText.observeAsState("")
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(
+            show = showDialog,
+            myTaskText = myTaskText,
+            onDismiss = { tasksViewModel.onDialogClose() },
+            onTaskAdded = { tasksViewModel.onTaskCreated() },
+            onTaskTextChanged = { tasksViewModel.onTaskTextChanged(it) }
+        )
+        FabDialog(
+            Modifier.align(Alignment.BottomEnd),
+            onNewTask = { tasksViewModel.onShowDialogClick() })
+        TasksList(tasksViewModel)
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTasksDialog(
+    show: Boolean,
+    myTaskText: String,
+    onDismiss: () -> Unit,
+    onTaskAdded: () -> Unit,
+    onTaskTextChanged: (String) -> Unit
+) {
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Añade tu tarea",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = myTaskText,
+                    onValueChange = { onTaskTextChanged(it) },
+                    singleLine = true,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(
+                    onClick = {
+                        onTaskAdded()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Añadir tarea")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
@@ -0,0 +1,32 @@
+package com.alopgal962.room.addtasks.ui
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TasksViewModel : ViewModel() {
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog: LiveData<Boolean> = _showDialog
+
+    private val _myTaskText = MutableLiveData<String>()
+    val myTaskText: LiveData<String> = _myTaskText
+
+    fun onDialogClose() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreated() {
+        onDialogClose()
+        Log.i("dam2", _myTaskText.value ?: "")
+        _myTaskText.value = ""
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
+
+    fun onTaskTextChanged(taskText: String) {
+        _myTaskText.value = taskText
+    }
+}
