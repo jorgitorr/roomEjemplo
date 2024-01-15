a6d984105a8edc907da4b5e823f9320f797af1a8
jorgitorr97 <jorgearceruiz97@gmail.com>
2024-01-16 00:11:01 +0100

    terminado parte 1

--- app/src/main/java/com/alopgal962/room/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TasksScreen.kt
@@ -1,16 +1,23 @@
 package com.alopgal962.room.addtasks.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -22,11 +29,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModel
+import com.alopgal962.room.addtasks.model.TaskModel
 
 /***
  * Este fichero contendrá la función Composable básica principal con el mismo nombre que el fichero y el resto de funciones Composable que necesitemos para la creación de nuestro diseño final
@@ -46,25 +55,28 @@ fun TasksScreen(tasksViewModel: TasksViewModel) {
             onTaskTextChanged = { tasksViewModel.onTaskTextChanged(it) }
         )
         FabDialog(
-            Modifier.align(Alignment.BottomEnd))
-            //onNewTask = { tasksViewModel.onShowDialogClick() })
-        //TasksList(tasksViewModel)
+            Modifier.align(Alignment.BottomEnd),
+            onNewTask = { tasksViewModel.onShowDialogClick() })
+        TasksList(tasksViewModel)
     }
 }
 
 
 @Composable
 fun FabDialog(
-    modifier: Modifier
+    modifier: Modifier,
+   onNewTask:()->Unit
 ) {
     FloatingActionButton(
         onClick = {
-            //Mostrar diálogo para añadir una tarea
+            onNewTask()
         }, modifier = modifier.padding(16.dp)
     ) {
         Icon(Icons.Filled.Add, contentDescription = "")
     }
 }
+
+
 
 
 
@@ -108,6 +120,67 @@ fun AddTasksDialog(
                     Text(text = "Añadir tarea")
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.tasks
+
+    LazyColumn {
+        //El parámetro opcional key ayuda a optimizar el LazyColumn
+        //Al indicarle que la clave es el id va a ser capaz de identificar cada tarea sin problemas
+        items(myTasks, key = { it.id }) { task ->
+            ItemTask(
+                task,
+                onTaskRemove = { tasksViewModel.onItemRemove(it) },
+                onTaskCheckChanged = { tasksViewModel.onCheckBoxSelected(it) }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ItemTask (
+    taskModel: TaskModel,
+    onTaskRemove: (TaskModel) -> Unit,
+    onTaskCheckChanged: (TaskModel) -> Unit
+) {
+    Card(
+        //pointerInput es una función se utiliza para configurar la entrada de puntero (input)
+        //para el componente visual al que se le aplica... la detección de gestos de entrada táctil
+        //En nuestro caso queremos eliminar una tarea con el gesto de pulsación larga (onLongPress)
+        //sobre la tarea, por lo tanto el componente visual dónde aplicar el modificador debe ser el Card.
+        //En la expresión lambda no podemos utilizar it como parámetro de la llamada a onTaskRemove(it)
+        //it es el Offset y nosotros necesitamos pasarle el taskModel que debe eliminarse...
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onLongPress = {
+                    onTaskRemove(taskModel)
+                })
+            },
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = taskModel.task,
+                modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .weight(1f)
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = { onTaskCheckChanged(taskModel) }
+            )
         }
     }
 }
--- app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
@@ -1,9 +1,11 @@
 package com.alopgal962.room.addtasks.ui
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.alopgal962.room.addtasks.model.TaskModel
 
 class TasksViewModel : ViewModel() {
     private val _showDialog = MutableLiveData<Boolean>()
@@ -11,15 +13,14 @@ class TasksViewModel : ViewModel() {
 
     private val _myTaskText = MutableLiveData<String>()
     val myTaskText: LiveData<String> = _myTaskText
+    //Los LiveData no van bien con los listados que se tienen que ir actualizando...
+    //Para solucionarlo, podemos utilizar un mutableStateListOf porque se lleva mejor con LazyColumn a la hora de refrescar la información en la vista...
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val tasks: List<TaskModel> = _tasks
+
 
     fun onDialogClose() {
         _showDialog.value = false
-    }
-
-    fun onTaskCreated() {
-        onDialogClose()
-        Log.i("dam2", _myTaskText.value ?: "")
-        _myTaskText.value = ""
     }
 
     fun onShowDialogClick() {
@@ -29,4 +30,38 @@ class TasksViewModel : ViewModel() {
     fun onTaskTextChanged(taskText: String) {
         _myTaskText.value = taskText
     }
+
+    //Actualizamos la función para crear la tarea en la lista anterior
+    //Comentamos el mensaje al log que hemos realizado inicialmente y añadimos una nueva tarea a la lista _tasks
+    fun onTaskCreated() {
+        onDialogClose()
+        //Log.i("dam2", _myTaskText.value ?: "")
+        _tasks.add(TaskModel(task = _myTaskText.value ?: ""))
+        _myTaskText.value = ""
+    }
+
+    fun onItemRemove(taskModel: TaskModel) {
+        //No podemos usar directamente _tasks.remove(taskModel) porque no es posible por el uso de let con copy para modificar el checkbox...
+        //Para hacerlo correctamente, debemos previamente buscar la tarea en la lista por el id y después eliminarla
+        val task = _tasks.find { it.id == taskModel.id }
+        _tasks.remove(task)
+    }
+
+    fun onCheckBoxSelected(taskModel: TaskModel) {
+        val index = _tasks.indexOf(taskModel)
+
+        //Si se modifica directamente _tasks[index].selected = true no se recompone el item en el LazyColumn
+        //Esto nos ha pasado ya en el proyecto BlackJack... ¿¿os acordáis?? :-(
+        //Y es que la vista no se entera que debe recomponerse, aunque realmente si se ha modificado el valor en el item
+        //Para solucionarlo y que se recomponga sin problemas en la vista, lo hacemos con un let...
+
+        //El método let toma como parámetro el objeto y devuelve el resultado de la expresión lambda
+        //En nuestro caso, el objeto que recibe let es de tipo TaskModel, que está en _tasks[index]
+        //(sería el it de la exprexión lambda)
+        //El método copy realiza una copia del objeto, pero modificando la propiedad selected a lo contrario
+        //El truco está en que no se modifica solo la propiedad selected de tasks[index],
+        //sino que se vuelve a reasignar para que la vista vea que se ha actualizado un item y se recomponga.
+        _tasks[index] = _tasks[index].let { it.copy(selected = !it.selected) }
+    }
+
 }
