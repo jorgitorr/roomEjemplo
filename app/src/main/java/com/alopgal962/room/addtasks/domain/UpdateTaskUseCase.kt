1b71ad70235225f43f1f6e07f1347fbb8036a632
AdrianPRG <alopgal962@g.educaand.es>
2024-01-19 19:54:47 +0100

    parte 2 terminada comparar cono la del profesor

---
+++ app/src/main/java/com/alopgal962/room/addtasks/domain/UpdateTaskUseCase.kt
@@ -0,0 +1,13 @@
+package com.alopgal962.room.addtasks.domain
+
+import com.alopgal962.room.addtasks.data.TaskEntity
+import com.alopgal962.room.addtasks.data.TaskRepository
+import com.alopgal962.room.addtasks.model.TaskModel
+import javax.inject.Inject
+
+class UpdateTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
+
+    suspend operator fun invoke(taskModel: TaskModel) {
+        taskRepository.update(taskModel)
+    }
+}
