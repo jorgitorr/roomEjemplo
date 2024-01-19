c7bd71684f524b2fad55ec87d27e748b3d749fdb
AdrianPRG <alopgal962@g.educaand.es>
2024-01-19 17:42:40 +0100

    hasta 7.2 parte 2

---
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TaskUiState.kt
@@ -0,0 +1,15 @@
+package com.alopgal962.room.addtasks.ui
+
+import com.alopgal962.room.addtasks.model.TaskModel
+
+/**
+ * Estados de la pantalla
+ */
+sealed interface TaskUiState {
+    //Si no recibes datos... usamos object
+    object Loading: TaskUiState
+    data class Error(val throwable: Throwable): TaskUiState
+    //La lista debe ser de elementos de tipo TaskModel porque estamos en la capa de UI.
+    //Recordad que TaskEntity solo va a ser accesible desde la capa de data y domain.
+    data class Success(val tasks:List<TaskModel>) : TaskUiState
+}
--- app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
@@ -5,9 +5,39 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alopgal962.room.addtasks.domain.AddTaskUseCase
+import com.alopgal962.room.addtasks.domain.GetTasksUseCase
 import com.alopgal962.room.addtasks.model.TaskModel
+import com.alopgal962.room.addtasks.ui.TaskUiState.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
 
-class TasksViewModel : ViewModel() {
+//El parámetro getTasksUseCase del constructor se inyecta sin private val porque no nos hace falta
+// ya que lo vamos a utilizar directamente en la variable uiState que gestionará los estados de la ui.
+@HiltViewModel
+class TasksViewModel @Inject constructor(
+    private val addTaskUseCase: AddTaskUseCase,
+    getTasksUseCase: GetTasksUseCase
+): ViewModel() {
+    //El caso de uso getTasksUseCase() nos devuelve el Flow continuo y cada vez que actualice
+    //los datos va a pasarlo a Success (ver data class en TaskUiState).
+    //Si por algún motivo falla y existe algún error, lo vamos a capturar y enviar al estado Error
+    //con el parámetro de la excepción que ha generado.
+    //El último modificador hará que cuando mi app o la pantalla esté en segundo plano hasta que no
+    //pasen 5 segundos no bloqueará o cancelará el Flow (por defecto es 0)
+    //Por ejemplo nuestra app pasará a segundo plano si nos llaman, o si desplegamos el menú superior
+    //para ver una notificación o un whatsapp, etc.
+    //Con stateIn, en el último argumento, también estamos asignando el estado inicial a Loading.
+    val uiState: StateFlow<TaskUiState> = getTasksUseCase().map(::Success)
+        .catch { Error(it) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
+
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
 
