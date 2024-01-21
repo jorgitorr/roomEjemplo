c986b02cee02b77f4a52cb84e3923b5b11e59b87
jorgitorr97 <jorgearceruiz97@gmail.com>
2024-01-21 23:57:53 +0100

    cambiado clase TasksViewModel

--- app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/ui/TasksViewModel.kt
@@ -76,36 +76,20 @@ class TasksViewModel @Inject constructor(
         viewModelScope.launch {
             addTaskUseCase(TaskModel(task = _myTaskText.value ?: ""))
         }
-
         _myTaskText.value = ""
     }
 
     fun onItemRemove(taskModel: TaskModel) {
-        //No podemos usar directamente _tasks.remove(taskModel) porque no es posible por el uso de let con copy para modificar el checkbox...
-        //Para hacerlo correctamente, debemos previamente buscar la tarea en la lista por el id y después eliminarla
-        //val task = _tasks.find { it.id == taskModel.id }
-        //_tasks.remove(task)
+        viewModelScope.launch {
+            deleteTaskUseCase(taskModel)
+        }
     }
 
     fun onCheckBoxSelected(taskModel: TaskModel) {
-        //val index = _tasks.indexOf(taskModel)
-
-        //Si se modifica directamente _tasks[index].selected = true no se recompone el item en el LazyColumn
-        //Esto nos ha pasado ya en el proyecto BlackJack... ¿¿os acordáis?? :-(
-        //Y es que la vista no se entera que debe recomponerse, aunque realmente si se ha modificado el valor en el item
-        //Para solucionarlo y que se recomponga sin problemas en la vista, lo hacemos con un let...
-
-        //El método let toma como parámetro el objeto y devuelve el resultado de la expresión lambda
-        //En nuestro caso, el objeto que recibe let es de tipo TaskModel, que está en _tasks[index]
-        //(sería el it de la exprexión lambda)
-        //El método copy realiza una copia del objeto, pero modificando la propiedad selected a lo contrario
-        //El truco está en que no se modifica solo la propiedad selected de tasks[index],
-        //sino que se vuelve a reasignar para que la vista vea que se ha actualizado un item y se recomponga.
-        //_tasks[index] = _tasks[index].let { it.copy(selected = !it.selected) }
-
         viewModelScope.launch {
-
+            updateTaskUseCase(taskModel.copy(selected = !taskModel.selected))
         }
     }
+
 
 }
