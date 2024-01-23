2aa67b8ee83f1c6bd82ec9b87ad94d2cce2fb89e
jorgitorr <jorgitorr97@gmail.com>
2024-01-23 17:52:59 +0100

    cambiados fallos taskDao

--- app/src/main/java/com/alopgal962/room/addtasks/data/TasksManageDatabase.kt
+++ app/src/main/java/com/alopgal962/room/addtasks/data/TasksManageDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase
 //TaskEntity::class te dará una referencia al objeto KClass que representa la clase TaskEntity.
 //Esto es útil, por ejemplo, cuando necesitas trabajar con la clase de manera dinámica,
 //como para obtener información sobre sus miembros, crear instancias de la clase, etc.
-@Database(entities = [TaskEntity::class], version = 1)
+@Database(entities = [TaskEntity::class], version = 1,exportSchema = false)
 abstract class TasksManageDatabase: RoomDatabase() {
     //DAO
     abstract fun taskDao():TaskDao
